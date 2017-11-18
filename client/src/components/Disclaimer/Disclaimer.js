33744897cf5f49fec96ded8c3c3494b7904f3fba
EliseoMejia <eliseofmejia@gmail.com>
2017-11-18 16:59:20 -0500

    disclaimer message and styling

--- client/src/components/Disclaimer/Disclaimer.js
+++ client/src/components/Disclaimer/Disclaimer.js
@@ -2,9 +2,21 @@ import React from "react";
 import "./Disclaimer.css";
 
 const Disclaimer = () => (
-	<footer className="footer">
-		<div className="container">
-			<p className="text-muted">Disclaimer that were are not responsible for any misinformation leading to tragic medical outcomes</p>
+	<footer className="footer ">
+		<div className="container text-center">
+			<p className="text-muted"><small>Disclaimer
+
+Last updated: November 18, 2017
+
+The information contained on miamigx website (the "Service") is for general information purposes only.
+
+Mi Amigx assumes no responsibility for errors or omissions in the contents on the Service.
+
+In no event shall Mi Amigx be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Mi Amigx reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.
+
+Mi Amigx does not warrant that the website is free of viruses or other harmful components.
+
+Disclaimer created by TermsFeed for Mi Amigx.</small></p>
 		</div>
 	</footer>
 );
