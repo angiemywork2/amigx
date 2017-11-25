4ed70a0f284abfc46309d0087fe9b7a392249aed
aragin <angiemywork2@yahoo.com>
2017-11-25 14:06:49 -0500

    added auth0

--- client/src/components/Navpills.js
+++ client/src/components/Navpills.js
@@ -4,17 +4,18 @@ import { Link } from "react-router-dom";
 const Navpills = () =>
   <ul className="nav nav-tabs">
     <li className={window.location.pathname === "/" ? "active" : ""}>
-      <Link to="/">Home \ Página de inicio</Link>
+      <Link to="/">Home</Link>
     </li>
     <li className={window.location.pathname === "/Health Tips" ? "active" : ""}>
-      <Link to="/tips">Health Tips \ Consejos de Salud </Link>
+      <Link to="/tips">Health Tips</Link>
     </li>
     <li className={window.location.pathname === "/otc" ? "active" : ""}>
-      <Link to="/otc">Over-The-Counter Medication \ Medicamentos de Venta Libre</Link>
+      <Link to="/otc">OTC Medication</Link>
     </li>
     <li className={window.location.pathname === "/medication" ? "active" : ""}>
-      <Link to="/medication">Medication \ Medicación</Link>
+      <Link to="/medication">Medication</Link>
     </li>
+   
     
   </ul>;
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import React, { Component } from "react";
+// import SearchForm from "../components/LoginForm";
+
+
+const Login = () =>
+  <div>
+    <h1 className="text-center">This is the login page</h1>
+    <p>
+      Welcome to the login page
+    </p>
+  </div>;
+
+export default Login;
