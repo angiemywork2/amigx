014ef0f96f4cbf9ee2efa2e0accca946b4d84d8d
EliseoMejia <eliseofmejia@gmail.com>
2017-11-27 20:50:54 -0500

    front-end list

--- client/src/pages/Medication.js
+++ client/src/pages/Medication.js
@@ -13,14 +13,23 @@ const Medication = () =>
     <Row>
       <Col size ="md-10, col-md-offset-1">
         <Jumbotron>
-          <h1>Medication Translator</h1>
-          <div class="panel panel-default">
-            <div class="panel-body">
-              Basic panel example
-            </div>
-          </div>
+          <h1 className="text-center">Medication Translator</h1>
+          <Row>
+            <Col size = "md-3, col-md-offset-1">
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  <h3 className="text-center">
+                    Use the form below to translate pharmacuetical prescriptions into spanish.
+                  </h3>
+                  <h3 className="text-center">
+                  click the example below to see where parts of the prescription are usually located.
+                  </h3>
+                </div>
+              </div>
+            </Col>
+          </Row>
           <div className="row">
-            <div className="col-md-6, col-md-offset-1">
+            <div className="col-md-6">
               <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo">Prescription Example</button>
               <div id="demo" className="collapse">
                 <div className="thumbnail">  
@@ -33,6 +42,31 @@ const Medication = () =>
             </div>
           </div>
         </Jumbotron>
+
+
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <form>
+                <Input name="PharmName" placeholder="Pharmacy Name (required)" />
+                <Input name="PharmAddress" placeholder="Pharmacy Address (required)" />
+                <Input name="PharmPhone" placeholder="Pharmacy Telephone Number (required)" />
+                <Input name="PharmDrugNum" placeholder="Pharmacy Drug ID Number (required)" />
+                <Input name="PharmFillDate" placeholder="Prescription Fill Date(required)" />
+                <Input name="DocName" placeholder="Doctor's Name (required)" />
+                <Input name="PatientName" placeholder="Patient Name (required)" />
+                <TextArea name="DrugInstruct" placeholder="Drug Instructions (required)" />
+                <Input name="DrugName" placeholder="Drug Name and Strength (required)" />
+                <Input name="DrugRefill" placeholder="Drug refills (required)"/>
+                <Input name="DrugUseByDate" placeholder="Use By Date (required)"/>
+
+                <FormBtn>Translate Prescription</FormBtn>
+                <br/>
+              </form>
+            </Jumbotron>
+          </Col>
+        </Row>
+        
       </Col>
     </Row>
   </Container>
