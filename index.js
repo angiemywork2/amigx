94090d8096a52f10ffa9455f31a2fe92e317912c
tapatterson <taneah.patterson@gmail.com>
2017-11-30 09:08:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as ReactIntl from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import intlEN from 'react-intl/locale-data/en';
+import intlES from 'react-intl/locale-data/es';
+import intlMessagesES from './components/i18n/locales/es.json';
+import intlMessagesEN from './components/i18n/locales/en.json';
+import EnglishBtn from './components/EnglishBtn/EnglishBtn';
+import SpanishBtn from './components/SpanishBtn/SpanishBtn';
+import registerServiceWorker from './registerServiceWorker';
+
+console.log("addLocaleData: ",addLocaleData);
+addLocaleData([...intlEN, ...intlES]);
+console.log("IntlProvider: ", IntlProvider);
+
+let i18nConfig = {
+    locale: 'en',
+    messages: intlMessagesEN
+};
+
+let changeLanguage = (lang) => {
+    i18nConfig = { locale: lang, messages: intlMessagesEN };
+    return i18nConfig;
+};
+
+let onChangeLanguage = (lang) =>{
+        switch (lang) {
+            case 'ES': i18nConfig.messages = intlMessagesES; break;
+            case 'EN': i18nConfig.messages = intlMessagesEN; break;
+            default: i18nConfig.messages = intlMessagesEN; break;
+        }
+        this.setState({ locale: lang });
+        i18nConfig.locale = this.state.locale;
+};
+
+
+ReactDOM.render(
+	
+	    
+	        <IntlProvider key={ i18nConfig.locale } locale={ i18nConfig.locale }  messages={ i18nConfig.messages }>
+	            <div>
+	                <SpanishBtn onChangeLanguage={this.onChangeLanguage} />
+	                <EnglishBtn onChangeLanguage={this.onChangeLanguage} />
+	                <App/>
+	                
+	            </div>
+	            
+	        </IntlProvider>,
+	         document.getElementById('root')
+	    );
+	
