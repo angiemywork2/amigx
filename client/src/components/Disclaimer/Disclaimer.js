9160309f553ef1dee9a9f70fa9a8f2630c9dc4f1
Dawn Mills <dawnwmills@aol.com>
2017-11-30 01:54:06 -0500

    formatted disclaimer and footer-d2

--- client/src/components/Disclaimer/Disclaimer.js
+++ client/src/components/Disclaimer/Disclaimer.js
@@ -2,23 +2,16 @@ import React from "react";
 import "./Disclaimer.css";
 
 const Disclaimer = () => (
-	<footer className="footer ">
-		<div className="container text-center">
-			<p className="text-muted"><small>Disclaimer
-
-Last updated: November 18, 2017
-
-The information contained on miamigx website (the "Service") is for general information purposes only.
-
-Mi Amigx assumes no responsibility for errors or omissions in the contents on the Service.
-
-In no event shall Mi Amigx be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Mi Amigx reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice.
-
-Mi Amigx does not warrant that the website is free of viruses or other harmful components.
-
-Disclaimer created by TermsFeed for Mi Amigx.</small></p>
+	<footer className="footer">
+		<div className="container text-justify">
+			<p className="text-muted"><strong>DISCLAIMER:</strong>
+				<br />Last updated: November 18, 2017
+				The information contained on miamigx website (the "Service") is for general information purposes only. Mi Amigx assumes no responsibility for errors or omissions in the contents on the Service. In no event shall Mi Amigx be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Mi Amigx reserves the right to make additions,deletions, or modification to the contents on the Service at any time without prior notice. Mi Amigx does not warrant that the website is free of viruses or other harmful components.
+				<br />Disclaimer created by TermsFeed for Mi Amigx.</p>
 		</div>
+			<p className="footer-company-name text-center">The Dream Team &copy; 2017</p>
 	</footer>
 );
 
 export default Disclaimer;
+
